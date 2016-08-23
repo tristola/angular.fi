9302ff21c64494ccf9ef18433c0537d49964e454
Jussi Kinnula <jussi.kinnula@frantic.com>
2016-08-23 10:42:58 +0300

    Use development NODE_ENV in webpack dev

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     devtool: 'cheap-source-map',
     plugins: [
         new DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            'process.env.NODE_ENV': JSON.stringify('development')
         }),
         new LiveReloadPlugin({
             appendScriptTag: true
